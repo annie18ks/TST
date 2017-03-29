886c724a61cd88cd3c83ba68d573616b42921fb8
Yuma Yagioka <kinntere@gmail.com>
2017-03-29 14:39:40 +0900

    no message

---
+++ MaliceUser.java
@@ -0,0 +1,29 @@
+public class MaliceUser {
+    private int Number;
+    private String accountID;
+    private String accountPW;
+
+    public void setNumber(int Number) {
+        this.Number = Number;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+    public void setAccountID(String accountID) {
+        this.accountID = accountID;
+    }
+
+    public String getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountPW(String accountPW) {
+        this.accountPW = accountPW;
+    }
+
+    public String getAccountPW() {
+        return accountPW;
+    }
+}
